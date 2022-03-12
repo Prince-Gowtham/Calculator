a128228d39d345ed543e5b701814a422638d7f73
Gowtham <30896453+Prince-Gowtham@users.noreply.github.com>
2022-03-12 12:30:10 +0530

    Add files via upload

---
+++ Calculator Program/main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+int main()
+{
+	int a,b;
+	scanf("%d%d", &a,&b);
+	printf("Add = %d\n", add(a,b));
+	printf("Sub = %d\n", sub(a,b));
+	printf("Mul = %d\n", mul(a,b));
+	printf("Div = %d\n", div(a,b));
+}
